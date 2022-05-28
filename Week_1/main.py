7f018f91a95245bd8c5def8b410dd17bcbf3c12e
Sephens <adesephens@gmail.com>
2022-05-29 00:10:54 +0300

    object and classes in python

---
+++ Week_1/main.py
@@ -0,0 +1,27 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self,name,age,tracks,score):
+        self.score = score
+        self.tracks = tracks
+        self.age = age
+        self.name
+
+    def change_name(name):
+        name =name
+    def Change_age(age):
+        age = age
+    def Add_track(track):
+        track =tracks
+
+    def get_score(score):
+        score = score
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
